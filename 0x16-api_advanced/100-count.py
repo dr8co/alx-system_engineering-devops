d51ed60e1b36339a463132e6ba24b93f0108f7ec
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-05-09 18:23:10 +0300

    Correction

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -7,7 +7,7 @@ print a sorted count of given keywords (case-insensitive, delimited by spaces.
 import requests
 
 
-def count_words(subreddit, word_list, after=None, word_count={}):
+def count_words(subreddit, word_list, words_found=[], after=None):
     """
     Prints a sorted count of given keywords (case-insensitive, delimited by
     spaces.
@@ -17,27 +17,26 @@ def count_words(subreddit, word_list, after=None, word_count={}):
     response = requests.get(url, headers=headers, allow_redirects=False,
                             params={"after": after})
     if after is None:
-        word_list = list(set([word.lower() for word in word_list]))
+        word_list = [word.lower() for word in word_list]
 
     if response.status_code == 200:
         for post in response.json().get("data").get("children"):
             title = post.get("data").get("title").lower()
-            words = title.split()
-            for word in word_list:
-                if word.lower() in words:
-                    if word in word_count:
-                        word_count[word] += 1
-                    else:
-                        word_count[word] = 1
-        after = response.json().get("data").get("after")
-        if after is None:
-            if not len(word_count) > 0:
-                print()
-                return
-            for key, value in sorted(word_count.items(),
-                                     key=lambda x: (-x[1], x[0])):
-                print("{}: {}".format(key, value))
-            return
-        return count_words(subreddit, word_list, after, word_count)
+            for word in title.split(' '):
+                if word in word_list:
+                    words_found.append(word)
+        aft = response.json().get("data").get("after")
+        if aft is not None:
+            count_words(subreddit, word_list, words_found, aft)
+        else:
+            result = {}
+            for word in words_found:
+                if word.lower() in result.keys():
+                    result[word.lower()] += 1
+                else:
+                    result[word.lower()] = 1
+            for key, value in sorted(result.items(), key=lambda x: x[1],
+                                     reverse=True):
+                print('{}: {}'.format(key, value))
     else:
         return
