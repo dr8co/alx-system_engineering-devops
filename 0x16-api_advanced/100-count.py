38a87608bb8b40130074ed330e599e0fc6cddb14
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-05-10 04:40:22 +0300

    Revert "Patch"
    This reverts commit 851e3dceea962701f872acc5bb87d66bea3400d8.

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,59 +1,42 @@
 #!/usr/bin/python3
-"""Query Reddit API to determine subreddit sub count
+"""
+Module to query the Reddit API, parse the title of all hot articles, and
+print a sorted count of given keywords (case-insensitive, delimited by spaces.
 """
 
 import requests
 
 
-def count_words(subreddit, word_list, count_list=[], next_page=None):
-    """Request subreddit recursively using pagination
+def count_words(subreddit, word_list, words_found=[], after=None):
     """
-    # convert word_list to dict with count
-    if not count_list:
-        for word in word_list:
-            count_list.append(dict({'keyword': word,
-                                    'count': 0}))
+    Prints a sorted count of given keywords (case-insensitive, delimited by
+    spaces.
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "dr8c00"}
+    response = requests.get(url, headers=headers, allow_redirects=False,
+                            params={"after": after})
+    if after is None:
+        word_list = [word.lower() for word in word_list]
 
-    # NETWORKING
-    # set custom user-agent
-    user_agent = '0x16-api_advanced-jmajetich'
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    # if page specified, pass as parameter
-    if next_page:
-        url += '?after={}'.format(next_page)
-
-    headers = {'User-Agent': user_agent}
-
-    r = requests.get(url, headers=headers, allow_redirects=False)
-
-    if r.status_code != 200:
-        return
-
-    # DATA PARSING
-    # load response unit from json
-    data = r.json()['data']
-
-    # extract list of pages
-    posts = data['children']
-    for post in posts:
-        title = post['data']['title']
-        for item in count_list:
-            title_lower = title.lower()
-            title_list = title_lower.split()
-            item['count'] += title_list.count(item['keyword'].lower())
-
-    next_page = data['after']
-    if next_page is not None:
-        return count_words(subreddit, word_list, count_list, next_page)
+    if response.status_code == 200:
+        for post in response.json().get("data").get("children"):
+            title = post.get("data").get("title").lower()
+            for word in title.split(' '):
+                if word in word_list:
+                    words_found.append(word)
+        aft = response.json().get("data").get("after")
+        if aft is not None:
+            count_words(subreddit, word_list, words_found, aft)
+        else:
+            result = {}
+            for word in words_found:
+                if word.lower() in result.keys():
+                    result[word.lower()] += 1
+                else:
+                    result[word.lower()] = 1
+            for key, value in sorted(result.items(), key=lambda x: x[1],
+                                     reverse=True):
+                print('{}: {}'.format(key, value))
     else:
-        # sort list by count
-        sorted_list = sorted(count_list,
-                             key=lambda word: (word['count'], word['keyword']),
-                             reverse=True)
-        keywords_matched = 0
-        # print keywords and counts
-        for word in sorted_list:
-            if word['count'] > 0:
-                print('{}: {}'.format(word['keyword'], word['count']))
-                keywords_matched += 1
         return
