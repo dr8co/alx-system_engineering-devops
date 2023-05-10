610cd62e5065bf7b1284ca8824e4c9aed9101151
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2023-05-10 05:49:41 +0300

    Rewrite word_count()

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -7,37 +7,35 @@ print a sorted count of given keywords (case-insensitive, delimited by spaces.
 import requests
 
 
-def count_words(subreddit, word_list, words_found=[], after=None):
+def count_words(subreddit, word_list, after="", word_dict={}):
     """
     Prints a sorted count of given keywords (case-insensitive, delimited by
     spaces.
     """
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "dr8c00"}
-    response = requests.get(url, headers=headers, allow_redirects=False,
-                            params={"after": after})
-    if after is None:
-        word_list = [word.lower() for word in word_list]
+    params = {"limit": 100, "after": after}
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code != 200:
+        return None
+    data = response.json().get("data")
+    after = data.get("after")
+    children = data.get("children")
+    for child in children:
+        title = child.get("data").get("title")
+        for word in word_list:
+            if word.lower() not in word_dict.keys():
+                word_dict[word.lower()] = 0
+            word_count = title.lower().split().count(word.lower())
+            word_dict[word.lower()] += word_count
 
-    if response.status_code == 200:
-        for post in response.json().get("data").get("children"):
-            title = post.get("data").get("title").lower()
-            for word in title.split(' '):
-                if word in word_list:
-                    words_found.append(word)
-        after = response.json().get("data").get("after")
-        if after is not None:
-            count_words(subreddit, word_list, words_found, after)
-        else:
-            result = {}
-            for word in words_found:
-                if word.lower() in result.keys():
-                    result[word.lower()] += 1
-                else:
-                    result[word.lower()] = 1
-            # sort by value, then by key
-            for key, value in sorted(result.items(),
-                                     key=lambda x: (-x[1], x[0])):
-                print('{}: {}'.format(key, value))
+    if after is None:
+        if len(word_dict) == 0:
+            return
+        for key, value in sorted(word_dict.items(),
+                                 key=lambda x: (-x[1], x[0])):
+            if value != 0:
+                print("{}: {}".format(key, value))
     else:
-        return
+        return count_words(subreddit, word_list, after, word_dict)
